1ae38fac177fa25c48ec1faf3c6acc3f3d0dd18e
Aadcode <118838917+Aadcode@users.noreply.github.com>
2024-11-10 15:21:20 +0530

    .gitignire modified

--- Backend/dist/db/db.js
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from "../../node_modules/.prisma/client/index.js";
-const prisma = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production")
-    global.prisma = prisma;
-export default prisma;
--- Backend/dist/routes/upload.Routes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { uploadFile } from "../controllers/upload.controller.js";
-import { upload } from "../middleware/fileHandler.middleware.js";
-const router = express.Router();
-router.post("/upload", upload.single("pdf"), uploadFile);
-export default router;
